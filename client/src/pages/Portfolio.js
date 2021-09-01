28b8f413c6ecf1414a6133f22024d5bff8f9051a
OrtizAlex <46968556+OrtizAlex@users.noreply.github.com>
2021-09-01 11:15:08 -0400

    edited piechart syntax and component conditional. added historial fetch request for line graph

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -5,16 +5,24 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
-import Chart from "../components/Chart";
+import PieChart from "../components/PieChart";
+import { getHistory } from "../utils/cryptoapi";
 
 
 const Portfolio = () => {
 
-    let transactions = [25, 30, 40, 10, 40];
+  const coinData = [
+    { name: "Bitcoin", value: 400 },
+    { name: "Ethereum", value: 300 },
+    { name: "Cardano", value: 300 },
+    { name: "Binance Coin", value: 200 },
+    { name: "Tether", value: 278 },
+    { name: "XRP", value: 189 }
+  ];
+
     let myChart;
 
     const { loading, data } = useQuery(QUERY_USER);
-    console.log(useQuery(QUERY_USER));
 
     const userData = data?.user || {};
 
@@ -29,20 +37,63 @@ const Portfolio = () => {
           padding: '0 30px',
         },
       });
+    
+    const renderPieChart = () => {
+      if (coinData.length !== 0){
+        return <h1>
+          <span>
+            <PieChart data={coinData}> </PieChart> 
+          </span>
+        </h1>;
+      }
+      else {
+        return <h1>
+          <span>
+            No coins have been purchased :(
+          </span>
+        </h1>;
+      }
+    }
+
+
+    const renderLineChart = () => {
+      const historyData = [];
+      //coinData.map((coin) => (historyData.push(getHistory(coin.name.toLowerCase().replace(/\s/g,'')))));
+      console.log(coinData.map((coin) => (historyData.push(getHistory(coin.name.toLowerCase().replace(/\s/g,''))))));
+      if (coinData.length !== 0){
+        return <h1>
+          <span>
+            <PieChart data={coinData}> </PieChart> 
+          </span>
+          
+        </h1>;
+      }
+      else {
+        return <h1>
+          <span>
+            No coins have been purchased :(
+          </span>
+        </h1>;
+      }
+    }
       
     const classes = useStyles();
 
     return (
+      
         <div>
+        <script src="https://widgets.coingecko.com/coingecko-coin-compare-chart-widget.js"></script>
+        <coingecko-coin-compare-chart-widget coin-ids={coinData.map((coin) => coin.name + ",")} locale="en"></coingecko-coin-compare-chart-widget>
+          
         <Jumbotron>
             
             <h1>{userData.firstName}'s Portfolio</h1>
-            <Avatar></Avatar>
-            <h1>
-            <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            <Chart props={transactions}> </Chart> 
-            </span>
-            </h1>
+            <Avatar className={classes.root}></Avatar>
+            {renderPieChart()}
+            {renderLineChart()}
+            {coinData.map((coin) => (<p>{coin.name} : {coin.value} coin(s)</p>))}
+            
+            
         </Jumbotron>
         </div>
     );
